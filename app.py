9909970c409f5fe145c4defd3df565366bdc351a
ichigar <ichigar@gobiernodecanarias.org>
2023-11-18 18:04:42 +0100

    v0.1

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
+import subprocess
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+def open_default_browser():
+    subprocess.Popen(['start', 'http://127.0.0.1:5000'], shell=True)
+     
+
 if __name__ == '__main__':
     app.run(debug=True)
+    open_default_browser()
--- run_app.py
+++ run_app.py
@@ -4,13 +4,13 @@ import os
 
 
 def run_flask_app():
-    subprocess.Popen([sys.executable, 'app.py'], cwd=os.path.abspath(os.path.dirname(__file__)))
+    subprocess.Popen([sys.executable, 'flask --app app.py run'], cwd=os.path.abspath(os.path.dirname(__file__)))
 
 
 def open_default_browser():
     subprocess.Popen(['start', 'http://127.0.0.1:5000'], shell=True)
-    
+
 
 if __name__ == '__main__':
-    run_flask_app()
+    # run_flask_app()
     open_default_browser()
